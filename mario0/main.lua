e787814543abc4661e51c05c28c8134d1f58a610
unknown <carcamo.gabriel@gmail.com>
2020-03-15 18:05:42 -0300

    bricks platform and sky in place

---
+++ mario0/main.lua
@@ -0,0 +1,37 @@
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243 
+
+Class = require 'class'
+push = require 'push'
+
+require 'Util'
+require 'Map'
+
+function love.load()
+  map = Map()
+
+  -- Screen setup
+  love.graphics.setDefaultFilter('nearest', 'nearest')
+  push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+    fullscreen = false,
+    resizable = false,
+    vsync = true
+  })
+
+end
+
+function love.update()
+
+end
+
+function love.draw()
+  push:apply('start')
+
+  love.graphics.clear(108/255, 140/255, 255/255, 255/255)
+  map:render()
+  
+  push:apply('end')
+end
